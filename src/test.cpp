00ab7bdd6ae313d711f9526c19d518223104b6b8
Thomas Van Gysegem <thomasvangysegem@gmail.com>
2018-05-30 17:36:47 +0200

    [add] RR/RL test cases

--- src/test.cpp
+++ src/test.cpp
@@ -77,7 +77,7 @@ bool test_CPU_NOP()
 
 bool test_CPU_LD_16bit()
 {
-    cpu->PC = 0;
+    init(0x00);
 
     uint8_t program[] = {
         0x01, 0x00, 0x11,   // LD BC, d16
@@ -125,7 +125,7 @@ bool test_BIN_RLC()
 
     execute({ 0xCB, 0x00 });    // RLC B
     ASSERT(cpu->reg[B] == 0b11100001);
-    ASSERT(cpu->reg[F] == 0x10);
+    ASSERT(cpu->reg[F] == 0x10);    // Test carry flag
     execute({ 0xCB, 0x00 });    // RLC B
     ASSERT(cpu->reg[B] == 0b11000011);
     execute({ 0xCB, 0x01 });    // RLC C
@@ -145,6 +145,11 @@ bool test_BIN_RLC()
     execute({ 0xCB, 0x06 });    // RLC (HL)
     ASSERT(mmu->get(cpu->reg16(HL)) == 0b11100001);
 
+    // Test zero flag
+    init(0x00);
+    execute({ 0xCB, 0x08 });
+    ASSERT(cpu->reg[F] == 0b10000000);
+
     return true;
 }
 
@@ -152,27 +157,100 @@ bool test_BIN_RRC()
 {
     init(0x0F);
 
-    execute({ 0xCB, 0x08 });
+    execute({ 0xCB, 0x08 });    // RRC B
     ASSERT(cpu->reg[B] == 0b10000111);
-    execute({ 0xCB, 0x08 });
+    execute({ 0xCB, 0x08 });    // RRC B
     ASSERT(cpu->reg[B] == 0b11000011);
-    execute({ 0xCB, 0x09 });
+    execute({ 0xCB, 0x09 });    // RRC C
     ASSERT(cpu->reg[C] == 0b10000111);
-    execute({ 0xCB, 0x0A });
+    execute({ 0xCB, 0x0A });    // RRC D
     ASSERT(cpu->reg[D] == 0b10000111);
-    execute({ 0xCB, 0x0B });
+    execute({ 0xCB, 0x0B });    // RRC E
     ASSERT(cpu->reg[E] == 0b10000111);
-    execute({ 0xCB, 0x0C });
+    execute({ 0xCB, 0x0C });    // RRC H
     ASSERT(cpu->reg[H] == 0b10000111);
-    execute({ 0xCB, 0x0D });
+    execute({ 0xCB, 0x0D });    // RRC L
     ASSERT(cpu->reg[L] == 0b10000111);
-    execute({ 0xCB, 0x0F });
+    execute({ 0xCB, 0x0F });    // RRC A
     ASSERT(cpu->reg[A] == 0b10000111);
-    ASSERT(cpu->reg[F] == 0x10);
+    ASSERT(cpu->reg[F] == 0x10);    // Test carry flag
+
+    mmu->set(cpu->reg16(HL), 0x0F);
+    execute({ 0xCB, 0x0E });    // RRC (HL)
+    ASSERT(mmu->get(cpu->reg16(HL)) == 0b10000111);
+
+    // Test zero flag
+    init(0x00);
+    execute({ 0xCB, 0x08 });
+    ASSERT(cpu->reg[F] == 0b10000000);
+
+    return true;
+}
+
+bool test_BIN_RL()
+{
+    init(0xF0);
+
+    execute({ 0xCB, 0x10 });
+    ASSERTV(cpu->reg[B] == 0b11100000, "B=0x%02X F=0x%02X\n", cpu->reg[B], cpu->reg[F]);
+    execute({ 0xCB, 0x10 });
+    ASSERT(cpu->reg[B] == 0b11000001);
+    execute({ 0xCB, 0x11 });
+    ASSERT(cpu->reg[C] == 0b11100001);
+    execute({ 0xCB, 0x12 });
+    ASSERT(cpu->reg[D] == 0b11100001);
+    execute({ 0xCB, 0x13 });
+    ASSERT(cpu->reg[E] == 0b11100001);
+    execute({ 0xCB, 0x14 });
+    ASSERT(cpu->reg[H] == 0b11100001);
+    execute({ 0xCB, 0x15 });
+    ASSERT(cpu->reg[L] == 0b11100001);
+    execute({ 0xCB, 0x17 });
+    ASSERT(cpu->reg[A] == 0b11100001);
+    ASSERT(cpu->reg[F] == 0x10);    // Test carry flag
+
+    mmu->set(cpu->reg16(HL), 0xF0);
+    execute({ 0xCB, 0x16 });
+    ASSERT(mmu->get(cpu->reg16(HL)) == 0b11100001);
+
+    // Test zero flag
+    init(0x00);
+    execute({ 0xCB, 0x10 });
+    ASSERT(cpu->reg[F] == 0b10000000);
+
+    return true;
+}
+
+bool test_BIN_RR()
+{
+    init(0x0F);
+
+    execute({ 0xCB, 0x18 });
+    ASSERT(cpu->reg[B] == 0b00000111);
+    execute({ 0xCB, 0x18 });
+    ASSERT(cpu->reg[B] == 0b10000011);
+    execute({ 0xCB, 0x19 });
+    ASSERT(cpu->reg[C] == 0b10000111);
+    execute({ 0xCB, 0x1A });
+    ASSERT(cpu->reg[D] == 0b10000111);
+    execute({ 0xCB, 0x1B });
+    ASSERT(cpu->reg[E] == 0b10000111);
+    execute({ 0xCB, 0x1C });
+    ASSERT(cpu->reg[H] == 0b10000111);
+    execute({ 0xCB, 0x1D });
+    ASSERT(cpu->reg[L] == 0b10000111);
+    execute({ 0xCB, 0x1F });
+    ASSERT(cpu->reg[A] == 0b10000111);
+    ASSERT(cpu->reg[F] == 0x10);    // Test carry flag
 
     mmu->set(cpu->reg16(HL), 0x0F);
     execute({ 0xCB, 0x0E });
     ASSERT(mmu->get(cpu->reg16(HL)) == 0b10000111);
+
+    // Test zero flag
+    init(0x00);
+    execute({ 0xCB, 0x18 });
+    ASSERT(cpu->reg[F] == 0b10000000);
 
     return true;
 }
@@ -185,6 +263,8 @@ int main(void)
     test("MMU: RAM check", &test_MMU_ram);
     test("Binary Operations: RLC", &test_BIN_RLC);
     test("Binary Operations: RRC", &test_BIN_RRC);
+    test("Binary Operations: RL", &test_BIN_RL);
+    test("Binary Operations: RR", &test_BIN_RR);
     test("CPU: NOP", &test_CPU_NOP);
     test("CPU: LD 16-Bit", &test_CPU_LD_16bit);
 
